7666dd7020c1ee6dabf113848c1e4a2639838195
Benjamin Eklund <101027885+BenEklundCS@users.noreply.github.com>
2023-10-24 15:49:58 +0000

    commenting puzzle.py for readability

--- src/puzzles.py
+++ src/puzzles.py
@@ -8,32 +8,43 @@ def memory_puzzle():
     """ Puzzle to test the players memory, forward path on crossroads """
     slow_print("You come across a stone room, with runes on the walls.")
     slow_print("Another adventurer's skeleton lies dead on the floor, looted long ago.")
+    # Player rolls a perception check
     roll = roll_d20()
     check_roll(roll, "perception")
     if roll > 10:
         slow_print("You realize this must be an ancient runic memory puzzle.")
     else:
         slow_print("You fail to determine the meaning of this room. Maybe you can still survive?")
+    # While puzzle is not completed...
     completed = False
     chars = []
     failures = 0
     while completed is False and len(chars) <= LENGTH_OF_PUZZLE:
+        # Generate a random letter and add it to the chars buffer
         letter = random_letter()
         chars.append(letter)
+        # Print the letter to the terminal
         print(letter)
         user_input = input()
+        # If the users input does not match the full buffer, add a failure
         if user_input != ''.join(chars):
             slow_print(f"\rThat is incorrect! You have {3-failures} more tries....................")
             failures = failures+1
+            # Call game_over() if they've failed too many times
             if failures == 4:
                 game_over()
+            # Clear the chars buffer
             chars.clear()
+        # Otherwise, the users input matches the full buffer
         else:
             slow_print("\rCorrect!")
+            # If the full buffer is equal to the LENGTH_OF_PUZZLE constant, they've won!
             if len(chars) == LENGTH_OF_PUZZLE:
                 completed = True
                 break
+        # Clear the terminal a few times
         clear_terminal_line() # clear output (correct/incorrect)
         clear_terminal_line() # clear player input
         clear_terminal_line() # clear given letter
+    # Exit puzzle with a true return
     return completed
--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
+""" settings.py contains global variables for various game settings """
 # Game settings
 
 # Used to enable/disable text blocks from story.py
